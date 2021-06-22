c3f43e24dc79a1c2131de58dea7edd254dbc125e
Amit Karwande <karwande@gmail.com>
2021-06-22 11:06:08 -0700

    initial commit

---
+++ src/main/java/com/github/gadzooks/multmodule/MultModuleApplication.java
@@ -0,0 +1,13 @@
+package com.github.gadzooks.multmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultModuleApplication.class, args);
+    }
+
+}
